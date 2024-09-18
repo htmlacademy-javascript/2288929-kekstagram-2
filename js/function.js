286f27dce1e7b4933df02f5b06726e5811a2f40f
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2024-09-18 14:57:23 +0300

    Добавляет альтернативную функцию проверки палиндрома

--- js/function.js
+++ js/function.js
@@ -1,6 +1,31 @@
-//Функция для проверки длины строки
+// Функция для проверки длины строки
 
 const lengthValidation = (string, length) => {
   const result = string.length <= length;
   return result;
 };
+
+lengthValidation('Капитан', 5);
+
+// Функция для проверки на палиндром
+
+const isPalindrom = (string) => {
+  const editString = string.replaceAll(' ', '').toLowerCase();
+  let reverseString = '';
+
+  for (let i = editString.length - 1; i >= 0; i--) {
+    reverseString += editString[i];
+  }
+
+  return reverseString === editString;
+};
+
+isPalindrom('Аргентина манит негра');
+
+// Функция для проверки на палиндром без цикла
+
+const isPalindromAlt = (string) => string.replaceAll(' ', '').toLowerCase() === string.replaceAll(' ', '').toLowerCase().split('').reverse().join('');
+
+isPalindromAlt('Около Миши молоко');
+
+
