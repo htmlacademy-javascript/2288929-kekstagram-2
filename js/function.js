a4ce1f86ae7c616dd10bebefed1582c8ab417304
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2024-09-18 21:24:24 +0300

    Добавляет функция изъятия числа

--- js/function.js
+++ js/function.js
@@ -25,4 +25,26 @@ const isPalindromAlt = (string) => string.replaceAll(' ', '').toLowerCase() === 
 
 isPalindromAlt('Около Миши молоко');
 
+// Функция изъятия чисел из строки
 
+const exportNumbres = (string) => {
+  let numberString = '';
+
+  if (typeof string === 'number') {
+    numberString = Math.abs(string);
+  }
+
+  for (let i = 0; i <= string.length; i++) {
+    if (parseInt(string[i], 10) || parseInt(string[i], 10) === 0) {
+      numberString += string[i];
+      numberString = Number(numberString);
+    }
+  }
+
+  if (numberString.length === 0 || Number.isNaN(Number(numberString))) {
+    numberString = NaN;
+  }
+  return numberString;
+};
+
+exportNumbres('1 кефир, 0.5 батона');
