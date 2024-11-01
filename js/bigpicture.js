1430b9ebcc9ee656470c69d3c5a4d584c861c59b
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2024-11-01 19:51:11 +0300

    Добавляет обработчики на открытие/закрытие фото

--- js/bigpicture.js
+++ js/bigpicture.js
@@ -1,8 +1,24 @@
+const body = document.querySelector('body');
 const bigPictureContainer = document.querySelector('.big-picture');
 const bigPicture = bigPictureContainer.querySelector('.big-picture__img');
+const bigPictureCloseButton = bigPictureContainer.querySelector('.big-picture__cancel');
 const likesCount = bigPictureContainer.querySelector('.likes-count');
 const description = bigPictureContainer.querySelector('.social__caption');
 const commentShownCount = bigPictureContainer.querySelector('.social__comment-shown-count');
 const commentTotalCount = bigPictureContainer.querySelector('.social__comment-total-count');
 const commentsContainer = bigPictureContainer.querySelector('.social__comments');
+const thumbnailsContainer = document.querySelector('.pictures');
 
+const bigPictureOpen = () => {
+  bigPictureContainer.classList.remove('hidden');
+  body.classList.add('modal-open');
+};
+
+const bigPictureClose = () => {
+  bigPictureContainer.classList.add('hidden');
+  body.classList.remove('modal-open');
+};
+
+thumbnailsContainer.addEventListener('click', bigPictureOpen);
+
+bigPictureCloseButton.addEventListener('click', bigPictureClose);
