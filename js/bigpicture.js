53df2933920979647bae6299de8f102ba849a6c2
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2024-11-01 19:29:57 +0300

    Добавляет модуль большого фото и константы

---
+++ js/bigpicture.js
@@ -0,0 +1,8 @@
+const bigPictureContainer = document.querySelector('.big-picture');
+const bigPicture = bigPictureContainer.querySelector('.big-picture__img');
+const likesCount = bigPictureContainer.querySelector('.likes-count');
+const description = bigPictureContainer.querySelector('.social__caption');
+const commentShownCount = bigPictureContainer.querySelector('.social__comment-shown-count');
+const commentTotalCount = bigPictureContainer.querySelector('.social__comment-total-count');
+const commentsContainer = bigPictureContainer.querySelector('.social__comments');
+
