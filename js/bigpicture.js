9a901f7dcf088c146caecb505adb45fda7378e6d
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2024-11-09 22:29:35 +0300

    Исправления для модульности, обработка клика

--- js/bigpicture.js
+++ js/bigpicture.js
@@ -1,4 +1,3 @@
-import { createPhotos, PHOTO_COUNT } from './gallery.js';
 const body = document.querySelector('body');
 const bigPictureContainer = document.querySelector('.big-picture');
 const bigPicture = bigPictureContainer.querySelector('.big-picture__img').querySelector('img');
@@ -10,9 +9,7 @@ const commentTotalCount = bigPictureContainer.querySelector('.social__comment-to
 const commentsCount = bigPictureContainer.querySelector('.social__comment-count');
 const commentsLoaderButton = bigPictureContainer.querySelector('.comments-loader');
 const commentsContainer = bigPictureContainer.querySelector('.social__comments');
-const thumbnailsContainer = document.querySelector('.pictures');
 
-const photos = createPhotos(PHOTO_COUNT);
 
 const onBigPictureKeydown = (evt) => {
   if (evt.key === 'Escape') {
@@ -22,11 +19,42 @@ const onBigPictureKeydown = (evt) => {
   }
 };
 
-const openBigPicture = () => {
+const renderComments = (comments) => {
+
+  comments.forEach(({avatar, name, message}) => {
+    const commentItem = document.createElement('li');
+    commentItem.classList.add('social__comment');
+
+    const avatarItem = document.createElement('img');
+    avatarItem.classList.add('social__picture');
+    avatarItem.src = avatar;
+    avatarItem.alt = name;
+    avatarItem.width = 35;
+    avatarItem.height = 35;
+
+    const avatarText = document.createElement('p');
+    avatarText.classList.add('social__text');
+    avatarText.textContent = message;
+
+    commentItem.append(avatarItem, avatarText);
+    commentsContainer.append(commentItem);
+  });
+};
+
+export const openBigPicture = (thumbnailData) => {
   bigPictureContainer.classList.remove('hidden');
   body.classList.add('modal-open');
   commentsCount.classList.add('hidden');
   commentsLoaderButton.classList.add('hidden');
+
+  bigPicture.src = thumbnailData.url;
+  descriptionBigPhoto.textContent = thumbnailData.description;
+  likesCountBigPhoto.textContent = thumbnailData.likes;
+  commentTotalCount.textContent = thumbnailData.comments.length;
+  commentShownCount.textContent = thumbnailData.comments.length;
+
+  commentsContainer.innerHTML = '';
+  renderComments(thumbnailData.comments);
 
   document.addEventListener('keydown', onBigPictureKeydown);
 };
@@ -39,44 +67,6 @@ const closeBipPicture = () => {
 
   document.removeEventListener('keydown', onBigPictureKeydown);
 };
-
-thumbnailsContainer.addEventListener('click', (evt) => {
-  openBigPicture();
-
-  const clickedThumbNail = evt.target.closest('.picture');
-  const photoId = clickedThumbNail.querySelector('.picture__img').dataset.id;
-  const photoData = photos[photoId - 1];
-  bigPicture.src = photoData.url;
-  descriptionBigPhoto.textContent = photoData.description;
-  likesCountBigPhoto.textContent = photoData.likes;
-  commentTotalCount.textContent = photoData.comments.length;
-  commentShownCount.textContent = photoData.comments.length;
-
-  const renderComments = (comments) => {
-
-    comments.forEach(({avatar, name, message}) => {
-      const commentItem = document.createElement('li');
-      commentItem.classList.add('social__comment');
-
-      const avatarItem = document.createElement('img');
-      avatarItem.classList.add('social__picture');
-      avatarItem.src = avatar;
-      avatarItem.alt = name;
-      avatarItem.width = 35;
-      avatarItem.height = 35;
-
-      const avatarText = document.createElement('p');
-      avatarText.classList.add('social__text');
-      avatarText.textContent = message;
-
-      commentItem.append(avatarItem, avatarText);
-      commentsContainer.append(commentItem);
-    });
-  };
-
-  commentsContainer.innerHTML = '';
-  renderComments(photoData.comments);
-});
 
 bigPictureCloseButton.addEventListener('click', () => {
   closeBipPicture();
