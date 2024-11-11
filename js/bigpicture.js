56b7e1cb82fe8887bb7a97c871813b41e36526d4
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2024-11-11 20:59:13 +0300

    Меняет способ создания комментариев

--- js/bigpicture.js
+++ js/bigpicture.js
@@ -11,6 +11,7 @@ const commentTotalCount = bigPictureContainer.querySelector('.social__comment-to
 const commentsCount = bigPictureContainer.querySelector('.social__comment-count');
 const commentsLoaderButton = bigPictureContainer.querySelector('.comments-loader');
 const commentsContainer = bigPictureContainer.querySelector('.social__comments');
+const templateComment = document.querySelector('#user-comment').content.querySelector('.social__comment');
 
 
 const onBigPictureKeydown = (evt) => {
@@ -21,26 +22,25 @@ const onBigPictureKeydown = (evt) => {
   }
 };
 
+const createComment = ({avatar, name, message}) => {
+  const comment = templateComment.cloneNode(true);
+
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+
 const renderComments = (comments) => {
+  const fragment = document.createDocumentFragment();
 
-  comments.forEach(({avatar, name, message}) => {
-    const commentItem = document.createElement('li');
-    commentItem.classList.add('social__comment');
-
-    const avatarItem = document.createElement('img');
-    avatarItem.classList.add('social__picture');
-    avatarItem.src = avatar;
-    avatarItem.alt = name;
-    avatarItem.width = 35;
-    avatarItem.height = 35;
-
-    const avatarText = document.createElement('p');
-    avatarText.classList.add('social__text');
-    avatarText.textContent = message;
-
-    commentItem.append(avatarItem, avatarText);
-    commentsContainer.append(commentItem);
+  comments.forEach((commentData) => {
+    const comment = createComment(commentData);
+    fragment.append(comment);
   });
+
+  commentsContainer.append(fragment);
 };
 
 export const openBigPicture = ({url, description, likes, comments}) => {
