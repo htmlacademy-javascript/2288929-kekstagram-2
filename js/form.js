e5be0ab48af4af4e94790942e5cc23a380f5998c
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2024-11-28 20:33:58 +0300

    Отменяет закрытие формы на Esc при фокусе в определенных инпутах

--- js/form.js
+++ js/form.js
@@ -1,21 +1,34 @@
+import { isEscapeKey } from './utils.js';
+
 const form = document.querySelector('.img-upload__form');
 const uploadInput = form.querySelector('.img-upload__input');
 const uploadOverlay = form.querySelector('.img-upload__overlay');
 const resetButton = form.querySelector('.img-upload__cancel');
 const effectLevelInput = form.querySelector('.effect-level__value');
+const hashtagInput = form.querySelector('.text__hashtags');
+const commentInput = form.querySelector('.text__description');
 
-const onResetButtonClose = () => {
+
+const onFormModalKeydown = (evt) => {
+  if (isEscapeKey(evt.key) && !(document.activeElement === hashtagInput || document.activeElement === commentInput)) {
+    evt.preventDefault();
+    onResetButtonClose();
+  }
+};
+
+function onResetButtonClose () {
   uploadOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
-  uploadInput.reset();
-  effectLevelInput.reset();
+  uploadInput.value = '';
 
   resetButton.removeEventListener('click', onResetButtonClose);
-};
+  document.removeEventListener('keydown', onFormModalKeydown);
+}
 
 uploadInput.addEventListener('change', () => {
   uploadOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
 
   resetButton.addEventListener('click', onResetButtonClose);
+  document.addEventListener('keydown', onFormModalKeydown);
 });
