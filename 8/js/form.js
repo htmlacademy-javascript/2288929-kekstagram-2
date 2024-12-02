4e39a969bbf303174f4b916bbb0391fe99e4898a
Keks <keks@htmlacademy.ru>
2024-12-02 22:34:19 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/form.js
+++ 8/js/form.js
@@ -6,22 +6,30 @@ const uploadOverlay = form.querySelector('.img-upload__overlay');
 const resetButton = form.querySelector('.img-upload__cancel');
 const hashtagInput = form.querySelector('.text__hashtags');
 const commentInput = form.querySelector('.text__description');
-
+const MAX_COMMENT_LENGTH = 140;
+const MAX_HASHTAG_COUNT = 5;
+const hastagRegex = /^#[a-zа-яё0-9]{1,19}$/i;
 
 const onFormModalKeydown = (evt) => {
-  if (isEscapeKey(evt.key) && !(document.activeElement === hashtagInput || document.activeElement === commentInput)) {
+  const disableCloseOnFocusInput = !(document.activeElement === hashtagInput || document.activeElement === commentInput);
+
+  if (isEscapeKey(evt.key) && disableCloseOnFocusInput) {
     evt.preventDefault();
     onResetButtonClose();
   }
 };
 
-function onResetButtonClose () {
+function closeform () {
   uploadOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
   uploadInput.value = '';
 
   resetButton.removeEventListener('click', onResetButtonClose);
   document.removeEventListener('keydown', onFormModalKeydown);
+}
+
+function onResetButtonClose () {
+  closeform();
 }
 
 uploadInput.addEventListener('change', () => {
@@ -39,53 +47,36 @@ const pristine = new Pristine(form, {
   errorTextClass: 'img-upload__field-wrapper--error'
 });
 
-const validateComment = (value) => value.length <= 140;
+const validateComment = (value) => value.length <= MAX_COMMENT_LENGTH;
 
-pristine.addValidator(commentInput, validateComment, 'Не более 140 символов');
+pristine.addValidator(commentInput, validateComment, `Не более ${MAX_COMMENT_LENGTH} символов`);
 
-const hastagRegex = /^#[a-zа-яё0-9]{1,19}$/i;
-
-const validateHashtagFormat = (input) => {
+const validateHashtag = (input) => {
   const trimmedInput = input.trim();
 
   if (trimmedInput === '') {
     return true;
   }
+  const hashtags = trimmedInput.split(/\s+/);
 
-  const hashtags = input.trim().split(/\s+/);
+  const isValidCount = () => hashtags.length <= MAX_HASHTAG_COUNT;
 
-  for (const hashtag of hashtags) {
-    if (!hastagRegex.test(hashtag)) {
-      return false;
+  const hasDuplicate = () => new Set(hashtags).size === hashtags.length;
+
+  const isValidFormat = () => {
+    for (const hashtag of hashtags) {
+      if (!hastagRegex.test(hashtag)) {
+        return false;
+      }
     }
-  }
 
-  return true;
+    return true;
+  };
+
+  return isValidCount() && hasDuplicate() && isValidFormat();
 };
 
-pristine.addValidator(hashtagInput, validateHashtagFormat, 'Хештег не соответствует формату');
-
-const validateHashtagCount = (input) => {
-  const hashtags = input.trim().split(/\s+/);
-
-  if (hashtags.length > 5) {
-    return false;
-  }
-
-  return true;
-};
-
-pristine.addValidator(hashtagInput, validateHashtagCount, 'Не более пяти хештегов');
-
-const validateHashtagDuplicates = (input) => {
-  const hashtags = input.trim().split(/\s+/);
-
-  const hasDuplicate = hashtags.some((hashtag, index) => hashtags.indexOf(hashtag) !== index);
-
-  return !hasDuplicate;
-};
-
-pristine.addValidator(hashtagInput, validateHashtagDuplicates, 'Хештеги не должны повторяться');
+pristine.addValidator(hashtagInput, validateHashtag, 'Хештеги не валидны');
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
