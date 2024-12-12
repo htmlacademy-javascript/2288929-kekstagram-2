8675748bea4a7dbdd0558792bbea1ef74d288610
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2024-12-12 21:17:26 +0300

    Добавляет функционал изменения размера загружаемого изображения

--- js/form.js
+++ js/form.js
@@ -3,6 +3,9 @@ import { isEscapeKey } from './utils.js';
 const MAX_COMMENT_LENGTH = 140;
 const MAX_HASHTAG_COUNT = 5;
 const HASHTAG_FORMAT_REGEX = /^#[a-zа-яё0-9]{1,19}$/i;
+const SCALE_VALUE_STEP = 25;
+const MIN_SCALE_VALUE = 25;
+const MAX_SCALE_VALUE = 100;
 
 const form = document.querySelector('.img-upload__form');
 const uploadInput = form.querySelector('.img-upload__input');
@@ -10,6 +13,34 @@ const uploadOverlay = form.querySelector('.img-upload__overlay');
 const resetButton = form.querySelector('.img-upload__cancel');
 const hashtagInput = form.querySelector('.text__hashtags');
 const commentInput = form.querySelector('.text__description');
+const scaleControlSmaller = form.querySelector('.scale__control--smaller');
+const scaleControlBigger = form.querySelector('.scale__control--bigger');
+const scaleControl = form.querySelector('.scale__control--value');
+const uploadImage = form.querySelector('.img-upload__preview').querySelector('img');
+
+const updateScaleValue = (newValue) => {
+  scaleControl.value = `${newValue}%`;
+  const imageScaleStyle = newValue / 100;
+  uploadImage.style.transform = `scale(${imageScaleStyle})`;
+};
+
+const scaleImageSmaller = () => {
+  let currentValue = parseInt(scaleControl.value, 10);
+
+  currentValue = currentValue > MIN_SCALE_VALUE ? currentValue - SCALE_VALUE_STEP : currentValue;
+  updateScaleValue(currentValue);
+};
+
+const scaleImageBigger = () => {
+  let currentValue = parseInt(scaleControl.value, 10);
+
+  currentValue = currentValue < MAX_SCALE_VALUE ? currentValue + SCALE_VALUE_STEP : currentValue;
+  updateScaleValue(currentValue);
+};
+
+const onScaleControlSmallerClick = () => scaleImageSmaller();
+
+const onScaleControlBiggerClick = () => scaleImageBigger();
 
 const onFormModalKeydown = (evt) => {
   const isTextInputActive = document.activeElement === hashtagInput || document.activeElement === commentInput;
@@ -27,6 +58,8 @@ function closeform () {
 
   resetButton.removeEventListener('click', onResetButtonClick);
   document.removeEventListener('keydown', onFormModalKeydown);
+  scaleControlSmaller.removeEventListener('click', onScaleControlSmallerClick);
+  scaleControlBigger.removeEventListener('click', onScaleControlBiggerClick);
 }
 
 function onResetButtonClick () {
@@ -39,6 +72,8 @@ uploadInput.addEventListener('change', () => {
 
   resetButton.addEventListener('click', onResetButtonClick);
   document.addEventListener('keydown', onFormModalKeydown);
+  scaleControlSmaller.addEventListener('click', onScaleControlSmallerClick);
+  scaleControlBigger.addEventListener('click', onScaleControlBiggerClick);
 });
 
 const pristine = new Pristine(form, {
