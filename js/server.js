4b1667fb1ed360a723902f55969023e1c7c6bcc5
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2025-01-15 18:58:18 +0300

    Добавляет сообщение об ошибки при загрузке

--- js/server.js
+++ js/server.js
@@ -1,8 +1,10 @@
+import { showDataError } from './utils';
+
 export const getUserPhotos = () =>
   fetch('https://31.javascript.htmlacademy.pro/kekstagram/data')
     .then((response) => {
       if (!response.ok) {
-        throw new Error ('ошибка загрузки');
+        showDataError();
       }
 
       return response.json();
--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,10 @@
 export const isEscapeKey = (key) => key === 'Escape';
+
+export const showDataError = () => {
+  const template = document.querySelector('#data-error').content.querySelector('.data-error');
+  document.body.insertAdjacentElement('beforeend', template);
+
+  setTimeout(() => {
+    template.remove();
+  }, 5000);
+};
