44d9c6ee86ea8c080792e752823182a735c619e1
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2025-02-19 11:51:31 +0300

    Упрощает функцию выбора 10 случаных фото

--- js/utils.js
+++ js/utils.js
@@ -2,21 +2,6 @@ const DEBOUNCE_DELAY = 500;
 
 export const isEscapeKey = (key) => key === 'Escape';
 
-export const shuffle = (array) => {
-  let m = array.length, t, i;
-  const filtredArr = [...array];
-
-  while (m) {
-    i = Math.floor(Math.random() * m--);
-
-    t = filtredArr[m];
-    filtredArr[m] = filtredArr[i];
-    filtredArr[i] = t;
-  }
-
-  return filtredArr;
-};
-
 export function debounce (callback, timeoutDelay = DEBOUNCE_DELAY) {
   let timeoutId;
 
