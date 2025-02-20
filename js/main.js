bfc90b42efd11f957b0397b02135768fa9ea9534
Olis Sotib <119186121+olissotib@users.noreply.github.com>
2025-02-20 18:32:48 +0300

    Исправляет после проверки

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 import './big-picture.js';
 import './form.js';
-import './filters.js';
+import { initFilters} from './filters.js';
 import { getData } from './server.js';
 import { initGallery } from './gallery.js';
-
-const filtersSection = document.querySelector('.img-filters');
-
-const initFilters = () => filtersSection.classList.remove('img-filters--inactive');
 
 getData()
   .then((photos) => {
     initGallery(photos);
-  })
-  .then(() => {
     initFilters();
   });
